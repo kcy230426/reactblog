db0e044bc3280f951203da77cd9dfde0a303771e
kcy230426 <purple_galaxy@naver.com>
2024-01-07 21:43:02 +0900

    첫번째 커밋

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h3 className="title">My blog</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
